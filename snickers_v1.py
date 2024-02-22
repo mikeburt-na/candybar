57070b68e29c3ae991435158613ce3c70b158c7d
mikeburt-na <mike.burt@netapp.com>
2024-02-22 17:03:01 -0600

    Update

---
+++ snickers_v1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from datetime import date
+
+root = Tk()
+root.title('Mike and Bonnie - Anniversaries Timer')
+root.iconbitmap('Images/Brian01.bmp')
+root.geometry("500x350")
+
+
+root.mainloop()
