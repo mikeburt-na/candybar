a6ba645359fb8a56cc231ffbed37dd30a97f8bdc
mikeburt-na <mike.burt@netapp.com>
2024-02-22 20:26:44 -0600

    update

--- snickers_v1.py
+++ snickers_v1.py
@@ -7,8 +7,10 @@ root.title('Mike and Bonnie - Anniversaries Timer')
 #root.geometry("720x480")
 root.attributes("-fullscreen", True)
 
-twix = Label(root, text="Mikey and Bonnie Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
+twix = Label(root, text="Mikey and Bonnie", font=("Helvetica", 42), bg="black", fg="white")
 twix.pack(pady=20, ipadx=10, ipady=10)
 
+payday = Label(root, text="Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
+payday.pack(pady=20, ipadx=10, ipady=10)
 
 root.mainloop()
