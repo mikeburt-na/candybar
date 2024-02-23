ab239f2af3eafc58daa488864509f0fda6214ca0
mikeburt-na <mike.burt@netapp.com>
2024-02-22 23:49:05 -0600

    update

--- snickers_v2.py
+++ snickers_v2.py
@@ -4,10 +4,20 @@ from datetime import datetime, timedelta
 class TimePassedApp:
     def __init__(self, master):
         self.master = master
-        self.master.title("Time Passed Since a Certain Date")
+        self.master.title('Mike and Bonnie - Anniversaries Timer')
+        self.master.attributes("-fullscreen", True)
 
-        self.label = Label(master, font=('Helvetica', 18))
-        self.label.pack(pady=20)
+        self.twix = Label(master, text="Mikey and Bonnie", font=("Helvetica", 42), bg="black", fg="white")
+        self.twix.pack(pady=20, ipadx=10, ipady=10)
+
+        self.payday = Label(master, text="Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
+        self.payday.pack(pady=20, ipadx=10, ipady=10)
+
+        self.time_label = Label(master, font=('Helvetica', 22))
+        self.time_label.pack(pady=20)
+
+        self.years_label = Label(master, font=('Helvetica', 22))
+        self.years_label.pack(pady=20)
 
         self.update_time_passed()
 
@@ -22,24 +32,21 @@ class TimePassedApp:
         years_passed = time_passed.days // 364.25
         years_passed_int = int(years_passed)
 
-        #time_passed_str = days_passed +  " days and " +  hours_passed + " hours have passed since " + start_date.strftime('%Y-%m-%d %H:%M:%S')
-        #time_passed_str = days_passed
-        time_passed_str = years_passed_int
-        self.years_label.config(text=time_passed_str)
+        self.years_label.config(text=years_passed_int)
         self.time_label.config(text=time_passed)
 
         self.master.after(60000, self.update_time_passed)  # Update every hour (3600000 milliseconds)
 
 def main():
     root = Tk()
-    root.title('Mike and Bonnie - Anniversaries Timer')
-    root.attributes("-fullscreen", True)
+    #root.title('Mike and Bonnie - Anniversaries Timer')
+    #root.attributes("-fullscreen", True)
 
-    twix = Label(root, text="Mikey and Bonnie", font=("Helvetica", 42), bg="black", fg="white")
-    twix.pack(pady=20, ipadx=10, ipady=10)
+    #twix = Label(root, text="Mikey and Bonnie", font=("Helvetica", 42), bg="black", fg="white")
+    #twix.pack(pady=20, ipadx=10, ipady=10)
 
-    payday = Label(root, text="Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
-    payday.pack(pady=20, ipadx=10, ipady=10)
+    #payday = Label(root, text="Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
+    #payday.pack(pady=20, ipadx=10, ipady=10)
     app = TimePassedApp(root)
     root.mainloop()
 
