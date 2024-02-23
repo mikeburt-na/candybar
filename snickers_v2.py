e939f29b46adb5f8d308db1ead29bf62667273a6
mikeburt-na <mike.burt@netapp.com>
2024-02-23 14:44:36 -0600

    update

--- snickers_v2.py
+++ snickers_v2.py
@@ -7,23 +7,34 @@ class TimePassedApp:
         self.master.title('Mike and Bonnie - Anniversaries Timer')
         self.master.attributes("-fullscreen", True)
 
+#   Header - Static Label
         self.twix = Label(master, text="Mikey and Bonnie", font=("Helvetica", 42), bg="black", fg="white")
         self.twix.pack(pady=20, ipadx=10, ipady=10)
 
         self.payday = Label(master, text="Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
         self.payday.pack(pady=20, ipadx=10, ipady=10)
 
+#   Dynamic Values
         self.time_label = Label(master, font=('Helvetica', 22))
         self.time_label.pack(pady=20)
 
         self.years_label = Label(master, font=('Helvetica', 22))
         self.years_label.pack(pady=20)
 
+        self.days_label = Label(master, font=('Helvetica', 22))
+        self.days_label.pack(pady=20)
+
+        self.hours_label = Label(master, font=('Helvetica', 22))
+        self.hours_label.pack(pady=20)
+
+#   Static Values
+        
+
         self.update_time_passed()
 
     def update_time_passed(self):
         # Date to compare against (Change this to your desired date)
-        start_date = datetime(2001, 2, 22, 0, 0, 0)
+        start_date = datetime(2001, 2, 21, 0, 0, 0)
         current_date = datetime.now()
 
         time_passed = current_date - start_date
@@ -33,6 +44,8 @@ class TimePassedApp:
         years_passed_int = "{} Years Since we Fell Asleep On the Couch ".format(int(years_passed))
 
         self.years_label.config(text=years_passed_int)
+        self.days_label.config(text=days_passed)
+        self.hours_label.config(text=hours_passed)
         self.time_label.config(text=time_passed)
 
         self.master.after(60000, self.update_time_passed)  # Update every hour (3600000 milliseconds)
