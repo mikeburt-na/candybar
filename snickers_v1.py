5b1ee5bc8b14f7892aa4671f42ddb909c543c97e
mikeburt-na <mike.burt@netapp.com>
2024-02-22 20:36:44 -0600

    update

--- snickers_v1.py
+++ snickers_v1.py
@@ -1,5 +1,18 @@
 from tkinter import *
-from datetime import date
+from datetime import datetime, timedelta
+
+def count_up_from_date(start_date_str, interval_seconds=1):
+    # Convert start_date_str to datetime object
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d %H:%M:%S')
+
+    # Define the interval
+    interval = timedelta(seconds=interval_seconds)
+
+    # Start counting
+    current_date = start_date
+    while True:
+        print(current_date.strftime('%Y-%m-%d %H:%M:%S'))
+        current_date += interval
 
 root = Tk()
 root.title('Mike and Bonnie - Anniversaries Timer')
@@ -12,5 +25,11 @@ twix.pack(pady=20, ipadx=10, ipady=10)
 
 payday = Label(root, text="Anniversaries Timer", font=("Helvetica", 42), bg="black", fg="white")
 payday.pack(pady=20, ipadx=10, ipady=10)
+
+butterfinger = "2001-02-23"
+snickers = "2003-06-21"
+reeses = count_up_from_date(butterfinger)
+
+milkyway = Label(root, text=reeses, font=("Helvetica", 42), bg="black", fg="white")
 
 root.mainloop()
