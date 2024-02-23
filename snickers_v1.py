3fa2469895d115b00cde1c6b489e9cecd32a62e9
mikeburt-na <mike.burt@netapp.com>
2024-02-22 19:47:49 -0600

    update

--- snickers_v1.py
+++ snickers_v1.py
@@ -4,7 +4,8 @@ from datetime import date
 root = Tk()
 root.title('Mike and Bonnie - Anniversaries Timer')
 #root.iconbitmap('Images/Brian01.bmp')
-root.geometry("720x480")
+#root.geometry("720x480")
+root.attributes("-fullscreen", True)
 
 
 root.mainloop()
