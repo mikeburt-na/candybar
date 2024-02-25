6f91ed5189c0cff33a17d1dc73cb3a89334ef91b
mikeburt-na <mike.burt@netapp.com>
2024-02-25 14:44:27 -0600

    u

--- snickers_v6.py
+++ snickers_v6.py
@@ -15,8 +15,8 @@ class TwoTablesApp:
         self.frame_table2.grid(row=1, column=0, sticky="nsew")
 
         # Configure grid weights to make both rows equally stretchable
-        self.master.rowconfigure(0, weight=1)
-        self.master.rowconfigure(1, weight=1)
+        #self.master.rowconfigure(0, weight=1)
+        #self.master.rowconfigure(1, weight=1)
         self.master.columnconfigure(0, weight=1)
         self.master.columnconfigure(1, weight=1)
 
