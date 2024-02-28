19e468b074308759839160e78a69e676e103a5f4
mikeburt-na <mike.burt@netapp.com>
2024-02-27 20:19:32 -0600

    u

--- snickers_v5.py
+++ snickers_v5.py
@@ -118,7 +118,10 @@ class FullScreenApp:
         dating_years_passed = dating_time_passed.days // 365.25
         dating_years_passed_int = "{} Years As of {}".format(int(dating_years_passed),format_date)
         # Dating Future
-        dating_next_ann = datetime((current_year + 1), 2, 23, 0, 0, 0)
+        if current_year == current_year:
+            dating_next_ann = datetime(current_year, 2, 23, 0, 0, 0)
+        else:
+            dating_next_ann = datetime((current_year + 1), 2, 23, 0, 0, 0)
         dating_delta = dating_next_ann - current_date
         dating_next_year_days = dating_delta.days
         dating_next_year_days_str = "{} More Days".format(int(dating_next_year_days))
@@ -137,7 +140,10 @@ class FullScreenApp:
         wed_years_passed_int = "{} Years As of {}".format(int(wed_years_passed),format_date)
 
         # Wedding Future
-        wed_next_ann = datetime((current_year + 1), 6, 21, 0, 0, 0)
+        if current_year == current_year:
+            wed_next_ann = datetime(current_year, 2, 23, 0, 0, 0)
+        else:
+            wed_next_ann = datetime((current_year + 1), 2, 23, 0, 0, 0)
         wed_delta = wed_next_ann - current_date
         wed_next_year_days = wed_delta.days
         wed_next_year_days_str = "{} More Days".format(int(wed_next_year_days))
